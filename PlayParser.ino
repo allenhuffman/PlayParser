3b0c44c9bd7810a7b5d9e3ddddb4e37116f6285c
Allen Huffman <alsplace@pobox.com>
2018-03-02 19:34:30 -0600

    Untested, but almost ready to start playing notes.

--- PlayParser.ino
+++ PlayParser.ino
@@ -22,6 +22,12 @@
 /*---------------------------------------------------------------------------*/
 // DEFINES
 /*---------------------------------------------------------------------------*/
+// 9C5B - Table of numerical note values for letter notes.
+const byte g_NoteJumpTable[7] =
+{
+  /*   A,  B, C, D, E, F, G */
+  /**/10, 12, 1, 3, 5, 6, 7 
+};
 
 /*---------------------------------------------------------------------------*/
 // GLOBALS
@@ -64,6 +70,8 @@ void play(const char *playString)
   char    *commandPtr;
   char    commandChar;
   bool    done;
+  byte    value;
+  byte    note;
 
   if (playString == NULL) return;
 
@@ -71,9 +79,9 @@ void play(const char *playString)
   commandPtr = (char*)playString;
 
   done = false;
-
   do
   {
+    
     // L9A43
     // L9B98
     // * GET NEXT COMMAND - RETURN VALUE IN ACCA
@@ -111,51 +119,155 @@ void play(const char *playString)
         // // ADJUST OCTAVE?
         // O - octave (1-5, default 2)
         //    Modifiers
-        g_Octave = checkModifier(&commandPtr, g_Octave);
-        
+        value = checkModifier(&commandPtr, g_Octave);
+        if (value >=1 && value <= 5)
+        {
+          g_Octave = value;
+        }
+        else
+        {
+          value = 0; // ?FC ERROR
+          done = true;
+        }
         break;
 
       // 9a6D
       case 'V':
         //  V - volume (1-31, default 15)
         //    Mofifiers
+        value = checkModifier(&commandPtr, g_Volume);
+        if (value >=1 && value <= 31)
+        {
+          g_Volume = value;
+        }
+        else
+        {
+          value = 0; // ?FC ERROR
+          done = true;
+        }
         break;
 
       // 9a8b
       case 'L':
         //  L - note length
         //    Modifiers
+        value = checkModifier(&commandPtr, g_NoteLn);
+        if (value > 0 )
+        {
+          g_NoteLn = value;
+        }
+        else
+        {
+          value = 0; // ?FC ERROR
+          done = true;
+        }
         //    . - dotted node
+        // DotVal = 0
+        // Can we keep adding dotted nodes?
+        // DotVal++
         break;
         
       // L9AB2
       case 'T':
         //  T - tempo (1-255, default 2)
         //    Modifiers
+        value = checkModifier(&commandPtr, g_Tempo);
+        if (value > 0)
+        {
+          g_Tempo = value;
+        }
+        else
+        {
+          value = 0; // ?FC ERROR
+          done = true;
+        }
         break;
         
       // L9AC3
       case 'P':
         //  P - pause (1-255)
-        //  ??? Modifiers ???
+        // get value
+        // pause for that amount?
         break;
 
       // L9AEB
       case 'N':
-        //  N - note (A-G, 1-12)
-        //    A-G
-        //      # - sharp
-        //      + - sharp
-        //      - - flat
-        break;
+        //  N - note (optional)
+        // Get next command character.
+        commandChar = getNextCommand(&commandPtr);
+        // Done if there is no more.
+        if (commandChar == '\0')
+        {
+          value = 0; // ?FC ERROR
+          done = true;
+          break;
+        }
+        // Drop down to looking for note value.
+        // no break
 
+      // L9AF2
       default:
+        // (A-G, 1-12)
+        //    A-G
+        if (commandChar >= 'A' || commandChar <= 'G')
+        {
+          // Get numeric note value of letter note. (0-11)
+          note = g_NoteJumpTable[commandChar - 'A'];
+          // note is now 1-12
+          
+          // Check for sharp/flat character.
+          commandChar = getNextCommand(&commandPtr);
+          
+          // Done if there is no more.
+          if (commandChar == '\0')
+          {
+            value = 0; // ?FC ERROR
+            done = true;
+            break;
+          }
+
+          //      # - sharp
+          //      + - sharp
+          if (commandChar == '#' || commandChar == '+') // Sharp
+          {
+            note++; // Add one to note number (charp)
+          }
+          else if (commandChar == '-') // Flat
+          {
+            //      - - flat
+            note--;
+          }
+        }
+        else
+        {
+          // L9BBE - Evaluate decimal expression in command string.
+          // Jump to cmp '=' thing in modifier!
+          note = checkForVariableOrNumeric(&commandPtr, commandChar);
+        }
+        
+        // L9B22 - Process note value.
+        note--; // Adjust note number, BASIC uses 1-2, internally 0-11
+        // If not was C (1), and was flat (0), this would make it 255.
+        if (note > 11)
+        {
+          value = 0; // ?FC ERROR
+          done = true;
+          break;
+        }
+
+        /*--------------------------------------------------------*/
+        // PROCESS NOTE HERE!
+        /*--------------------------------------------------------*/
+        
         break;
     } // end switch( commandChar );
 
-    Serial.print(commandChar);
-
   } while( done == false );
+
+  if (value == 0)
+  {
+    Serial.println(F("?FC ERROR"));
+  }
 
   Serial.println(F("End."));
 } // end of play()
@@ -213,7 +325,6 @@ char getNextCommand(char **ptr)
 byte checkModifier(char **ptr, byte value)
 {
   char      commandChar;
-  uint16_t  temp; // MUL A*B = D
   
   if ((ptr != NULL) && (*(*ptr) != '\0'))
   {
@@ -269,67 +380,88 @@ byte checkModifier(char **ptr, byte value)
         }
         break;
 
-      // SirSound will not have a way to support this.
-
-      // CHECK FOR VARIABLE EQUATE
-      case '=':
-        // "=XX;" - value = whatever XX is set to.
-        // Skip until semicolon or end of string.
-        do
-        {
-          commandChar = getNextCommand(ptr);
-          if (commandChar == '\0') break;
-          
-        } while( commandChar != ';' );
-        break;
-
-      // Else, check for numeric string.
+      // Could be = or number, so we call a separate function since we
+      // need this in the note routine as well.
       default:
-        temp = 0;
-        value = 0;
-        do
-        {
-          // Stop at a non-numeric character.
-          if ( isdigit(commandChar) == 0) // not digit
-          {
-            // Rewind so we end up where we started.
-            ptr--;
-            break;
-          }
-
-          // Base 10. First time it will be 0 * 10.
-          temp = temp * 10;
-          // Convert ASCII number to value.
-          temp = temp + (commandChar - '0');
-
-          if (temp>255)
-          {
-            // In BASIC we would ?FC ERROR.
-            // Skip past numbers.
-            do
-            {
-              commandChar = getNextCommand(ptr);
-              if (commandChar == '\0') break;
-            } while(isdigit(commandChar) != 0); // digit
-
-            // Rewind
-            ptr--;
-            temp = 0;
-            break;
-          }
-          // Get another command byte.
-          commandChar = getNextCommand(ptr);  
-        } while( commandChar != '\0' );
-
-        value = temp;
+        value = checkForVariableOrNumeric(ptr, commandChar);
         break;
-    
+        
     } // end of switch( commandChar )
 
   } // end of NULL check
 
   return value;  
 } // end of checkModifiers()
+
+
+byte checkForVariableOrNumeric(char **ptr, char commandChar)
+{
+  byte      value;
+  uint16_t  temp; // MUL A*B = D
+
+  switch( commandChar )
+  {
+    // SirSound will not have a way to support this.
+  
+    // CHECK FOR VARIABLE EQUATE
+    case '=':
+      // "=XX;" - value = whatever XX is set to.
+      // Skip until semicolon or end of string.
+      do
+      {
+        commandChar = getNextCommand(ptr);
+        if (commandChar == '\0') break;
+        
+      } while( commandChar != ';' );
+      
+      value = 0; // ?FC ERROR since we do not support this year.
+      break;
+  
+    // Else, check for numeric string.
+    default:
+      temp = 0;
+      value = 0;
+      do
+      {
+        // Stop at a non-numeric character.
+        if ( isdigit(commandChar) == 0) // not digit
+        {
+          // Rewind so we end up where we started.
+          ptr--;
+          break;
+        }
+  
+        // Base 10. First time it will be 0 * 10.
+        temp = temp * 10;
+        // Convert ASCII number to value.
+        temp = temp + (commandChar - '0');
+  
+        if (temp>255)
+        {
+          // In BASIC we would ?FC ERROR.
+          // Skip past numbers.
+          do
+          {
+            commandChar = getNextCommand(ptr);
+            if (commandChar == '\0') break;
+          } while(isdigit(commandChar) != 0); // digit
+  
+          // Rewind
+          ptr--;
+          temp = 0;
+          break;
+        }
+        // Get another command byte.
+        commandChar = getNextCommand(ptr);  
+      } while( commandChar != '\0' );
+  
+      value = temp;
+      break;
+  } // end of switch( commandChar )
+
+  return value;
+}
+
 
 /*---------------------------------------------------------------------------*/
 // End of PlayParser
